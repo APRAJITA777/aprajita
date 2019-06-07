e7fbc6cdfa40499e31caa9009845e3a0014f67a3
unknown <aprajiasingh94306@gmail.com>
2019-06-07 14:15:34 +0530

    first commit

---
+++ app/src/main/java/com/example/dialing/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.dialing;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.net.Uri;
+import android.text.TextUtils;
+import android.content.Intent;
+
+
+public class MainActivity extends  AppCompatActivity() {
+EditText text;
+Button Dial
+
+    @override
+    protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    text=(EditText)findViewByid(R.id.)
+  }
+}
